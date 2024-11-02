0a645364c56ec15c199789259a6ce4e5bd8cd106
186485 <kasper.austboe@gmail.com>
2024-11-02 18:19:34 +0100

    Oppgave 3+4
    Ferdig med obligatoriske oppgaver

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,15 +1,44 @@
 package no.hvl.dat100.oppgave4;
 
+import java.io.*;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-import no.hvl.dat100.common.TODO;
+import no.hvl.dat100.common.*;
+import no.hvl.dat100.oppgave1.*;
 import no.hvl.dat100.oppgave3.*;
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+		
+		PrintWriter skriver = null;
+		try {
+			File fil = new File(mappe, filnavn);
+			skriver = new PrintWriter(fil);
+			
+			Innlegg[] innleggTabell=samling.getSamling();
+			
+			for (int i=0; i<samling.getAntall();i++) {
+				if(innleggTabell[i]!=null) {
+					skriver.println(innleggTabell[i].toString());
+				}
+			}
+			return true;
+		}catch(IOException e) {
+			e.printStackTrace();
+			return false;
+		}finally {
+			if(skriver!=null) {
+				skriver.close();
+			}
+		}
+		
+		
+		
+		
+		
+	
 
-		throw new UnsupportedOperationException(TODO.method());
 	}
 }
